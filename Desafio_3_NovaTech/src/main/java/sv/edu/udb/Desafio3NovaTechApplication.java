27d5db2d3011e0a8aa24f4cbf2430e11094466a2
JonathanSeguraXP <jonathansegura.cv@gmail.com>
2025-11-04 09:50:40 -0600

    First Commit

---
+++ Desafio_3_NovaTech/src/main/java/sv/edu/udb/Desafio3NovaTechApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio3NovaTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio3NovaTechApplication.class, args);
+	}
+
+}
